59c5bdab7c8a42543e1c25aa8ddfb2c95fc4718c
Antonio Cruz <antonio.cruz5612@gmail.com>
2024-11-19 13:36:42 -0800

    start the server on port 3000.

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ const port = 3000;
 // Serve static files from the 'assets' folder.
 app.use(express.static(path.join(__dirname, 'assets')));
 
-// Serve the landing page when the rool URL is accessed.
+// Serve the landing page when the root URL is accessed.
 app.get ('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Start the server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
