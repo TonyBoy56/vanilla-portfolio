9aab085e6f7725defef9283212dd1135f6f96f68
Antonio Cruz <antonio.cruz5612@gmail.com>
2024-11-19 13:34:40 -0800

    call app to use static files from the 'assets' and serve the landing page when the root URL is accessed.

--- server.js
+++ server.js
@@ -3,3 +3,12 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+// Serve static files from the 'assets' folder.
+app.use(express.static(path.join(__dirname, 'assets')));
+
+// Serve the landing page when the rool URL is accessed.
+app.get ('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
